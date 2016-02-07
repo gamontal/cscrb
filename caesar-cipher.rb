43dfe30ddd801cfab66e8e76109a52985607a1cb
Gabriel Montalvo <gmontalvoriv@users.noreply.github.com>
2016-02-06 22:28:39 -0400

    trivial changes

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,5 +1,3 @@
-# Simple Ruby implementation of the Caesar substitution cipher
-
 $ALPHA = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 module CaesarCipher
   def encrypt(key, plaintext)
